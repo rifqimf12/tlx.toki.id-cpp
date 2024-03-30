338cd5ffd58d52e92c6915c834d77d3702b35738
rifqimf12 <rifqimf120@gmail.com>
2024-03-30 20:48:52 +0700

    add 03/f

---
+++ 03/f.cpp
@@ -0,0 +1,7 @@
+#include<cstdio>
+
+int main(){
+    int a, b, c, d, e, f, g, h, i;
+    scanf("%d %d %d %d %d %d %d %d %d",&a, &b, &c, &d, &e, &f, &g, &h, &i);
+    printf("%d %d %d\n%d %d %d\n%d %d %d\n",a, d, g, b, e, h, c, f, i);
+}
